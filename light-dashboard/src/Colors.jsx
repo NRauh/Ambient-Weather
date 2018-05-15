10cb51b03994a1fa8550c2a667a88abf726da546
Nate Rauh <nrauh@users.noreply.github.com>
2018-05-15 00:02:07 -0500

    Fill out pages

--- light-dashboard/src/Colors.jsx
+++ light-dashboard/src/Colors.jsx
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import { List, ListItem } from 'material-ui/List';
+import Dialog from 'material-ui/Dialog';
+import ColorPicker from './ColorPicker';
 
 export default class Colors extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  toggleDialog = () => this.setState({ open: !this.state.open });
+
   render() {
     return (
       <div>
         <h1>Color Selection</h1>
 
-        <ul>
-          <li>Clear</li>
-          <li>Few Clouds</li>
-          <li>Scattered Clouds</li>
-          <li>Broken CLouds</li>
-          <li>Shower Rain</li>
-          <li>Rain</li>
-          <li>Thunderstore</li>
-          <li>Snow</li>
-          <li>Mist</li>
-          <li>Sever Weather</li>
-        </ul>
+        <List>
+          <ListItem primaryText="Clear" onClick={this.toggleDialog} />
+          <ListItem primaryText="Few Clouds" />
+          <ListItem primaryText="Scattered Clouds" />
+          <ListItem primaryText="Broken Clouds" />
+          <ListItem primaryText="Shower Rain" />
+          <ListItem primaryText="Rain" />
+          <ListItem primaryText="Thunderstorm" />
+          <ListItem primaryText="Snow" />
+          <ListItem primaryText="Mist" />
+          <ListItem primaryText="Sever Weather" />
+        </List>
 
         <RaisedButton label="Apply" primary />
         <RaisedButton label="Cycle" secondary />
         <RaisedButton label="Manual" secondary />
+
+        <Dialog
+          open={this.state.open}
+          onRequestClose={this.toggleDialog}
+        >
+          <ColorPicker />
+        </Dialog>
       </div>
     );
   }
