ee8ad5c3848dfe163571b4a34882e28aee9ac622
Nate Rauh <nrauh@users.noreply.github.com>
2018-05-15 21:57:16 -0500

    Layout weather page

--- light-dashboard/src/Weather.jsx
+++ light-dashboard/src/Weather.jsx
@@ -1,29 +1,75 @@
 import React, { Component } from 'react';
+import { List, ListItem } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 
 export default class Weather extends Component {
   render() {
+    const styles = {
+      container: {
+        display: 'grid',
+        gridTemplateAreas: `
+          "icon info"
+          "forecast forecast"
+        `
+      },
+      weatherIcon: {
+        gridArea: 'icon',
+      },
+      weatherInfo: {
+        gridArea: 'info',
+      },
+      weatherForecast: {
+        gridArea: 'forecast',
+      },
+    };
+
     return (
-      <div>
-        <div className="left">
-          <img src="https://placekitten.com/400/400" alt="Weather Icon" />
-        </div>
-        <div className="right">
-          <h1>Location</h1>
-          <h2>68 - Sunny</h2>
+      <div style={styles.container}>
+        <div style={styles.weatherIcon}>
+          <img
+            src="https://placekitten.com/250/250"
+            alt="Weather Icon"
+          />
         </div>
 
-        <div className="full">
+        <div className="right" style={styles.weatherInfo}>
+          <h1>68 - Sunny</h1>
+          <h2>Location</h2>
+        </div>
+
+        <div className="full" style={styles.weatherForecast}>
           <hr className="currentColor" />
 
-          <ul>
-            <li>00:00 - Color</li>
-            <li>04:00 - Color</li>
-            <li>08:00 - Color</li>
-            <li>12:00 - Color</li>
-            <li>16:00 - Color</li>
-            <li>20:00 - Color</li>
-            <li>00:00 - Color</li>
-          </ul>
+          <List>
+            <ListItem
+              primaryText="00:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="04:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="08:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="12:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="16:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="20:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+            <ListItem
+              primaryText="00:00 - Condition"
+              leftAvatar={<Avatar backgroundColor="grey" />}
+            />
+          </List>
         </div>
       </div>
     );
