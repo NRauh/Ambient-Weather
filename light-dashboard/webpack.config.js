a5b7ddd1469c0eb4382ee399edf2610c2362400e
Nate Rauh <nrauh@users.noreply.github.com>
2018-05-15 22:57:44 -0500

    Eject React
    Wanted more control over building, so I ejected create-react-app

---
+++ light-dashboard/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './public/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
