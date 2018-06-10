c883f69929a94726f0d2420f20f5456e2b144f92
Nate Rauh <nrauh@users.noreply.github.com>
2018-06-10 17:56:35 -0500

    Change to blue theme

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -5,10 +5,26 @@ import './index.css';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 import { ACTIONS } from './store';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import lightblue from '@material-ui/core/colors/lightBlue';
+import red from '@material-ui/core/colors/red';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightblue[800],
+    },
+    secondary: {
+      main: red[700],
+    },
+  },
+});
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
