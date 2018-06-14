113276e30b84c385249fa8369047f37f4a2cf27a
Nate Rauh <nrauh@users.noreply.github.com>
2018-06-13 22:07:27 -0500

    Remove weather icon
    The weather icon was removed, since I didn't have the time to find a
    set, and wasn't sure if a set would be too large or not

--- dashboard/src/weather-page/WeatherPage.tsx
+++ dashboard/src/weather-page/WeatherPage.tsx
@@ -1,30 +1,38 @@
 import * as React from 'react';
-import './Weather.css';
-import { WeatherIcon } from './WeatherIcon';
 import { WeatherInfo } from './WeatherInfo';
 import { WeatherForecast } from './WeatherForecast';
 import { DashboardState, WeatherState } from '../store';
 import { connect } from 'react-redux';
-import { ConditionList } from '@light/types';
+import { ConditionList, Color } from '@light/types';
+import { Divider } from '@material-ui/core';
 
 interface WeatherPageProps extends WeatherState {
   colors: ConditionList,
 }
 
-const WeatherPage = (props: WeatherPageProps) => (
-  <div className="weather">
-    <WeatherIcon />
-    <WeatherInfo
-      temperature={props.current.temperature}
-      condition={props.current.condition}
-      time={props.current.time}
-    />
-    <WeatherForecast
-      forecast={props.forecast}
-      colors={props.colors}
-    />
-  </div>
-);
+const WeatherPage = (props: WeatherPageProps) => {
+  const currentColor: Color = props.colors[props.current.condition] || {};
+  const dividerStyles = {
+    backgroundColor: `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`,
+  };
+
+  return (
+    <div className="weather">
+      <WeatherInfo
+        temperature={props.current.temperature}
+        condition={props.current.condition}
+        time={props.current.time}
+      />
+
+      <Divider style={dividerStyles} />
+
+      <WeatherForecast
+        forecast={props.forecast}
+        colors={props.colors}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state: DashboardState) => {
   return {
